ba28f7f2eca313a9d4554e51434125b3e38d0222
ruanalbuquerquefreitas <ruan.albuquerque.freitas@escola.pr.gov.br>
2026-08-25 15:22:10 +0000

    asasasasasasa

--- acesso-ponto.js
+++ acesso-ponto.js
@@ -5,7 +5,12 @@ const cliente = {
   email: "andre@dominio.com",
 };
 
-console.log(`O nome do cliente é ${cliente.nome} e essa pessoa tem ${cliente.idade} anos.`
+console.log(
+  `O nome do cliente é ${cliente["nome"]} e essa pessoa tem ${cliente["idade"]} anos.`
 );
 
-console.log(`Os 3 primeiros digitos do CPF são ${cliente.cpf.substring(0, 3)}`);
+const chaves = ["nome", "idade", "cpf", "email", "altura"];
+
+chaves.forEach((chave) => {
+  console.log(`A chave ${chave} tem valor ${cliente[chave]}`);
+});
